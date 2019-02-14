0dfd099f4df71ca1ba7f90fcbc3952bd9924726e
lordlycat <lordly_cat@163.com>
2019-02-15 01:57:29 +0800

    build

---
+++ build/precache-manifest.9febcb6b231d2f504320685838266bb2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1003cc508a48338251da6e10c27c79de",
+    "url": "./static/media/chooseBackground.1003cc50.png"
+  },
+  {
+    "revision": "1eb2153438980e4a17d8efe4d5a9adb6",
+    "url": "./static/media/bullet.1eb21534.png"
+  },
+  {
+    "revision": "2d3b980be6bb54f5e74b43ecca4e8642",
+    "url": "./static/media/background.2d3b980b.png"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "8227fd1a51a6184bb594",
+    "url": "./static/js/main.8227fd1a.chunk.js"
+  },
+  {
+    "revision": "d7a5189203872954d561",
+    "url": "./static/js/1.d7a51892.chunk.js"
+  },
+  {
+    "revision": "8227fd1a51a6184bb594",
+    "url": "./static/css/main.96f89768.chunk.css"
+  },
+  {
+    "revision": "bc79fcaaa49065ed52f7f99ac69b687b",
+    "url": "./index.html"
+  }
+];
